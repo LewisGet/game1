7258bad0c4242cb90ad2d420949cfbeac558317c
Lewis Jang <lewis.aedes@gmail.com>
2017-01-19 15:54:18 +0800

    add game over function, and fixed some bugs

--- plugins/ljMain.js
+++ plugins/ljMain.js
@@ -1,1 +1,14 @@
-exports.ljGame = require('ljSystem');
+var kernel = require('ljSystem');
+
+exports.init = kernel;
+
+events.entityDeath(function (event) {
+    var entity = event.getEntity();
+
+    if (entity == kernel.ljGame.player)
+    {
+        entity.chat("game over");
+        entity.chat("source: " + kernel.ljGame.source.toString() + " kills.");
+        kernel.ljGame.game_timer.cancel();
+    }
+});
